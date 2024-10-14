f1759efc47ffdf44ce93abf1ad1f68b0e1901f67
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2024-10-14 16:11:24 +0200

    Problema ruta update_tools

--- trabajo/bin/update_tools.sh
+++ trabajo/bin/update_tools.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# Guardamos el directorio actual para volver a él al final
+wd=$(pwd)
+cd
+
 echo "Actualizando syncwin en /bin"
 cp trabajo/bin/syncwin /bin 
 
 echo "Actualizando permisos de syncwin"
 chown bin /bin/syncwin
 chmod 755 /bin/syncwin
+
+cd $wd

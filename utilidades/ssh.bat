2a2b7c806657f3414a4980572c079b64b25f82be
Joaquín Entrialgo <jentrialgo@gmail.com>
2024-10-14 11:33:08 +0200

    Actualizar ssh para que funcione con la nueva versión de Windows

--- utilidades/ssh.bat
+++ utilidades/ssh.bat
@@ -3,4 +3,4 @@ set TERM=xterm
 @rem puerto 5522
 @rem conexión con @localhost
 @rem establece los algoritmos de cifrado e intercambio de claves
-ssh -p 5522 -oKexAlgorithms=+diffie-hellman-group1-sha1 usuario@localhost
+ssh -p 5522 -oKexAlgorithms=+diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-dss -o MACs=hmac-sha1 usuario@localhost

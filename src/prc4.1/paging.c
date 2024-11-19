aef0b0d3eb74890e9ad6416a675c7d00f64bfdcf
Joaquín Entrialgo <jentrialgo@gmail.com>
2024-11-19 19:12:17 +0100

    Ficheros de la práctica 4.1

---
+++ src/prc4.1/paging.c
@@ -0,0 +1,17 @@
+
+#include "e9_system.h" /* e9_printf */
+#include "kernel.h"
+#include <sys/vm.h> /* I386_VM_PRESENT, ... , I386_CR0_PG */
+
+typedef unsigned pte_t;
+typedef unsigned pde_t;
+
+#define BIOS_DATE 0x0FFFFF5
+#define CEIL_DIV(x, y) (((x) + (y) - 1) / (y))
+#define MEM_SIZE_MB 128
+
+void setup_paging(void)
+{
+    e9_printf("\n*** En setup_paging() ***\n");
+
+}
